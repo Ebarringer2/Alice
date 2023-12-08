6f992d3df46fcddf90663c4ba9b477630668750e
Elliott Barringer <elliottbarringer@icloud.org>
2023-12-07 22:36:33 -0800

    fixed bug

--- alice/src/main.rs
+++ alice/src/main.rs
@@ -28,7 +28,7 @@ fn find(mut f_dir: &str) -> bool {
         f_dir = "."; //default arg - the current directory
     }
     match Command::new("explorer")
-        .arg(!format("\"{}\""),f_dir) 
+        .arg(format!("\"{}\"", f_dir)) 
         .spawn()   // spawning the process
     {
         Ok(_) => {
